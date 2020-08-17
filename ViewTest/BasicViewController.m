4bee583a15140a6163c3a02b7c323c3ab368ee20
qicaitiankong <zihaoshuhan@163.com>
2020-08-17 15:04:06 +0800

    增加图片文件，基本控制器

---
+++ ViewTest/BasicViewController.m
@@ -0,0 +1,32 @@
+//
+//  BasicViewController.m
+//  ViewTest
+//
+//  Created by Lizihao Li on 2020/8/17.
+//  Copyright © 2020 Lizihao Li. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+@interface BasicViewController ()
+
+@end
+
+@implementation BasicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
